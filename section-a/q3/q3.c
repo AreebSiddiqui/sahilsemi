c349e3c346b520e27446e9843971df8c2f15c107
AreebSiddiqui <areeb40@hotmail.com>
2020-08-19 11:51:17 +0500

    q3.v3-added-module-c

--- section-a/q3/q3.c
+++ section-a/q3/q3.c
@@ -45,23 +45,41 @@ void module_a_get_house5(int *val) {
 }
 }
 
+int module_c_cal_all_area(int *val) {
+	static int total_area=0;	
+	//For getting pointer to house_1
+	struct House *houseptr;
+	houseptr=container_of(val, struct House, area);
+	struct House *pointer = houseptr;
+
+	list_for_each_entry(pointer, &houselist, list){
+		total_area = total_area+pointer->area;
+	}
+	return total_area;
+}
+
+
 //Starting Module
 static int __init start(void) {
 	
 	struct House *house_1 = kzalloc(sizeof(struct House), GFP_KERNEL);
 	add_in_list(house_1, 1, "red"); 
-	// int *val = &house_1->area;	
 
 	struct House *house_2 = kzalloc(sizeof(struct House), GFP_KERNEL);
 	add_in_list(house_2, 2, "blue");
 
 	struct House *house_3 = kzalloc(sizeof(struct House), GFP_KERNEL);
-	add_in_list(house_3, 3, "green");	//For getting pointer to house_1
+	add_in_list(house_3, 3, "green");	
 
 	struct House *house_4 = kzalloc(sizeof(struct House), GFP_KERNEL);
 	add_in_list(house_4, 4, "pink");
+
+//Function calling	// int *val = &house_1->area;//ref pg# 44
 	
 	module_a_get_house5(&house_1->area);
+	int result = module_c_cal_all_area(&house_1->area);
+	printk(KERN_ALERT "Total area is %d", result);
+	
 	return 0;
 }
 
